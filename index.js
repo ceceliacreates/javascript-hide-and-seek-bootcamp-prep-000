14fb3ef451d1f780369820231f0c4599d146c8c2
Cecelia Smith <ceceliacreates@gmail.com>
2019-01-11 21:47:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget(target) {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+const lis = document.querySelectorAll('.ranked-list li')
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+}
+}
